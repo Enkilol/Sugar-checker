a0086aa7038af9ddaaf429d40c5ed574bf764af7
Enkilol <87070269+Enkilol@users.noreply.github.com>
2023-03-14 23:04:47 +0100

    Update calcolatore.js

--- calcolatore.js
+++ calcolatore.js
@@ -13,7 +13,7 @@ const gramsPerPortion = nutritionFacts.serving_quantity; // Grammi per porzione
 
 const productImage = document.createElement('img');
 productImage.src = imageUrl;
-document.head.appendChild(productImage);
+document.body.appendChild(productImage);
 
 // Mostra solo le informazioni sugli zuccheri
 document.getElementById("product-name").innerHTML = productName;
