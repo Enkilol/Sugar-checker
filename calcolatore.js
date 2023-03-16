5128a3072ee33346e3ec10bf5311c4e4067c2040
Enkilol <87070269+Enkilol@users.noreply.github.com>
2023-03-16 22:25:51 +0100

    Update calcolatore.js

--- calcolatore.js
+++ calcolatore.js
@@ -4,21 +4,28 @@ const code = queryString.get("code");
 fetch(`https://world.openfoodfacts.org/api/v0/product/${code}.json?lang=it`)
 .then(response => response.json())
 .then(data => {
-const imageUrl = data.product.image_url;
-const productName = data.product.product_name;
-const nutritionFacts = data.product.nutriments;
-const sugarPerPortion = nutritionFacts.sugars_per_portion;
-const sugarPerPortionDivided = sugarPerPortion / 4; // Dividi per 4
-const gramsPerPortion = nutritionFacts.serving_quantity; // Grammi per porzione
+  const imageUrl = data.product.image_url;
+  const productName = data.product.product_name;
+  const nutritionFacts = data.product.nutriments;
+  const sugarPerPortion = nutritionFacts.sugars_per_portion;
+  const sugarPerPortionDivided = sugarPerPortion / 4; // Dividi per 4
+  const gramsPerPortion = nutritionFacts.serving_quantity; // Grammi per porzione
 
-const productImage = document.createElement('img');
-productImage.src = imageUrl;
-document.body.appendChild(productImage);
+  const productImage = document.createElement('img');
+  productImage.onload = () => {
+    // L'immagine è stata caricata correttamente
+    document.getElementById("product-image").src = imageUrl;
+  }
+  productImage.onerror = () => {
+    // Errore durante il caricamento dell'immagine
+    console.error("Impossibile caricare l'immagine del prodotto");
+  }
+  productImage.src = imageUrl;
+  document.body.appendChild(productImage);
 
-// Mostra solo le informazioni sugli zuccheri
-document.getElementById("product-Image").src = productImage;
-document.getElementById("product-name").innerHTML = productName;
-document.getElementById("cucchiaini").innerHTML = `Contiene ${sugarPerPortionDivided} cucchiai di zucchero per una porzione.`;
-document.getElementById("grammi-per-porzione").innerHTML = `Una porzione di prodotto equivale a ${gramsPerPortion} grammi`;
+  // Mostra solo le informazioni sugli zuccheri
+  document.getElementById("product-name").innerHTML = productName;
+  document.getElementById("cucchiaini").innerHTML = `Contiene ${sugarPerPortionDivided} cucchiai di zucchero per una porzione.`;
+  document.getElementById("grammi-per-porzione").innerHTML = `Una porzione di prodotto equivale a ${gramsPerPortion} grammi`;
 })
 .catch(error => console.error(error));
